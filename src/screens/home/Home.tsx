81c786c6f13b0bfed33f65b88c5852302e961055
Tomer Halali <tomerh@webtech-inv.co.il>
2023-08-07 12:54:01 +0300

    adding product mini card

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,34 +1,68 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import homeStyle from "./Home.module.scss";
-import ProductModal from "../../components/product/productModal/ProductModal";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import OurServices from "../../components/ourServices/OurServices";
+import ProductMiniCard from "../../components/product/productMiniCard/ProductMiniCard";
 
 const Home = () => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const product = {
-        name: "גחל יאקוזה לנרגילה",
-        images: [
-            "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442559/extra_large/db04dd95075c786c32661df1351125d5.jpg",
-            "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442560/extra_large/d813c5a8e62a07af4f76add6959d46b0.jpg"
-        ],
-        stock: true,
-        price: "39.99",
-        promotionPrice: ""
-    }
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
-    return (
-        <div className={homeStyle.container}>
-            <ProductModal product={product}/>
-            <Header isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen}/>
-            <div className={homeStyle.homePage}>
-                Home Page
-            </div>
-            <OurServices />
-            <Footer/>
-        </div>
-    );
+  const products = [
+    {
+      name: "1גחל יאקוזה לנרגילה",
+      images: [
+        "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442559/extra_large/db04dd95075c786c32661df1351125d5.jpg",
+        "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442560/extra_large/d813c5a8e62a07af4f76add6959d46b0.jpg",
+      ],
+      stock: false,
+      price: "19.99",
+      promotionPrice: "",
+    },
+    {
+      name: "2גחל יאקוזה לנרגילה",
+      images: [
+        "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442559/extra_large/db04dd95075c786c32661df1351125d5.jpg",
+        "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442560/extra_large/d813c5a8e62a07af4f76add6959d46b0.jpg",
+      ],
+      stock: true,
+      price: "29.99",
+      promotionPrice: "",
+    },
+    {
+      name: "3גחל יאקוזה לנרגילה",
+      images: [
+        "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442559/extra_large/db04dd95075c786c32661df1351125d5.jpg",
+        "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442560/extra_large/d813c5a8e62a07af4f76add6959d46b0.jpg",
+      ],
+      stock: false,
+      price: "39.99",
+      promotionPrice: "",
+    },
+    {
+      name: "4גחל יאקוזה לנרגילה",
+      images: [
+        "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442559/extra_large/db04dd95075c786c32661df1351125d5.jpg",
+        "https://d3m9l0v76dty0.cloudfront.net/system/photos/9442560/extra_large/d813c5a8e62a07af4f76add6959d46b0.jpg",
+      ],
+      stock: true,
+      price: "49.99",
+      promotionPrice: "",
+    }
+  ]
+
+  return (
+    <div className={homeStyle.container}>
+      <Header isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen} />
+      <div className={homeStyle.homePageNewProducts}>
+        {products.map((product)=>{
+          return <ProductMiniCard product={product} />
+        })}
+      </div>
+      <OurServices />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
