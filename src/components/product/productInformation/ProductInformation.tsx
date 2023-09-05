433a5b589f3aa1689eb727674a1bccbeef1e22c0
Tomer Halali <tomerh@webtech-inv.co.il>
2023-09-05 16:05:52 +0300

    fit product information to mobile

--- src/components/product/productInformation/ProductInformation.tsx
+++ src/components/product/productInformation/ProductInformation.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
 import SliderShow from "../../elements/sliderShow/SliderShow";
 import productInformationStyle from "./ProductInformation.module.scss";
 import Divider from "@mui/material/Divider";
 import Chip from "@mui/material/Chip";
 import Product from "../../../models/Product.model";
 import { observer } from "mobx-react";
-
 import rootStores from "../../../stores";
 import { CART_STORE } from "../../../stores/consts";
 import CartStore from "../../../stores/Cart.store";
@@ -31,76 +29,58 @@ const ProductInformation = observer((props: IProps) => {
   };
 
   return (
-    <div className={productInformationStyle.productInformationContainer}>
-      <Grid container columns={12}>
-        <Grid item xs={6}>
-          <div className={productInformationStyle.gridDetailsContainer}>
-            <div>
-              <div className={productInformationStyle.productTitleContainer}>
-                <p>{product.name}</p>
-              </div>
-              <div className={productInformationStyle.productPriceContainer}>
-                {product.promotionPrice !== "" ? (
-                  <div>
-                    <span>₪{product.promotionPrice}</span>
-                    <span className={productInformationStyle.strikePrice}>
-                      ₪{product.price}
-                    </span>
-                  </div>
-                ) : (
-                  <span>₪{product.price}</span>
-                )}
-              </div>
-              <div className={productInformationStyle.dividerContainer}>
-                <Divider
-                  className={productInformationStyle.customerDivider}
-                  variant="middle"
-                />
-              </div>
-              <div className={productInformationStyle.indicatorContainer}>
-                <Grid container columns={12}>
-                  <Grid item xs={4}>
-                    {product.inStock ? (
-                      <Chip
-                        label="זמין במלאי"
-                        color="success"
-                        variant="outlined"
-                      />
-                    ) : (
-                      <Chip
-                        label="אזל מהמלאי"
-                        color="warning"
-                        variant="outlined"
-                      />
-                    )}
-                  </Grid>
-                </Grid>
-              </div>
-              <div className={productInformationStyle.addToCartButtonContainer}>
-                <Button
-                  className={productInformationStyle.customButtonStyle}
-                  size="small"
-                  onClick={handleAddToCart}
-                  variant="outlined"
-                  disabled={!product.inStock}
-                >
-                  הוסף לעגלה
-                </Button>
-              </div>
-              <div
-                className={productInformationStyle.productDescriptionContainer}
-              >
-                <p>{product?.description}</p>
-              </div>
-            </div>
-          </div>
-        </Grid>
-        <Grid item xs={6}>
-          <div className={productInformationStyle.gridImagesContainer}>
-            <SliderShow images={product.images} />
-          </div>
-        </Grid>
-      </Grid>
+    <div
+      className={`${productInformationStyle.productInformationContainer} row`}
+    >
+      <div className="col-12 col-md-6 row">
+        <div className={`${productInformationStyle.productTitleContainer} ${productInformationStyle.customColumn}`}>
+          <span>{product.name}</span>
+        </div>
+        <div className={`${productInformationStyle.productPriceContainer} ${productInformationStyle.customColumn}`}>
+          {product.promotionPrice !== "" ? (
+            <>
+              <span>₪{product.promotionPrice}</span>
+              <span className={productInformationStyle.strikePrice}>
+                ₪{product.price}
+              </span>
+            </>
+          ) : (
+            <span>₪{product.price}</span>
+          )}
+        </div>
+        <div className={`${productInformationStyle.dividerContainer} ${productInformationStyle.customColumn}`}>
+          <Divider
+            className={productInformationStyle.customerDivider}
+            variant="middle"
+          />
+        </div>
+        <div className={`${productInformationStyle.inStockContainer} ${productInformationStyle.customColumn}`}>
+          {product.inStock ? (
+            <Chip className={productInformationStyle.customChip} label="זמין במלאי" color="success" variant="outlined" />
+          ) : (
+            <Chip className={productInformationStyle.customChip} label="אזל מהמלאי" color="warning" variant="outlined" />
+          )}
+        </div>
+        <div className={`${productInformationStyle.addToCartButtonContainer} ${productInformationStyle.customColumn}`}>
+          <Button
+            className={productInformationStyle.customButtonStyle}
+            size="small"
+            onClick={handleAddToCart}
+            variant="outlined"
+            disabled={!product.inStock}
+          >
+            הוסף לעגלה
+          </Button>
+        </div>
+        <div className={`${productInformationStyle.productDescriptionContainer} ${productInformationStyle.customColumn}`}>
+          <p>{product?.description}</p>
+        </div>
+      </div>
+      <div className="col-8 col-md-6">
+        <div className={productInformationStyle.gridImagesContainer}>
+          <SliderShow images={product.images} />
+        </div>
+      </div>
     </div>
   );
 });
