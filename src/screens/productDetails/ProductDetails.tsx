d27386fe08f3e3441d18cbcb3ab917acf934ec56
Tomer Halali <tomerh@webtech-inv.co.il>
2023-09-11 09:37:24 +0300

    fit component to mobile

--- src/screens/productDetails/ProductDetails.tsx
+++ src/screens/productDetails/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import productDetailsStyle from "./ProductDetails.module.scss";
 import ProductInformation from "../../components/product/productInformation/ProductInformation";
 import { useLocation } from "react-router-dom";
@@ -6,6 +6,11 @@ import { useLocation } from "react-router-dom";
 const ProductDetails = () => {
   const location = useLocation();
   const product = location.state.product;
+
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top of the page on route change
+  }, []);
 
   return (
     <div className={productDetailsStyle.productDetailsContainer}>
