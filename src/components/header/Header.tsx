c293b7cc4376c9e26a5b47d7018ebc487af931f4
Matan Balestra <matan.balestra@gmail.com>
2023-09-14 13:58:39 +0300

    get adds text from the server

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,72 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import { observer } from 'mobx-react';
+import React, {useEffect, useState} from 'react';
+import {observer} from 'mobx-react';
 import '../elements/popup/popupStyle.css';
 import './Header.css';
 import headerStyle from './Header.module.scss';
 import HeaderActions from './headerActions/HeaderActions';
 import Sections from './sections/Sections';
-import { animated, useTransition } from '@react-spring/web';
-import { useNavigate } from 'react-router-dom';
+import {animated, useTransition} from '@react-spring/web';
+import {useNavigate} from 'react-router-dom';
 import Title from './title/Title';
 import SearchIcon from '../search/SearchIcon';
-import { useMediaQuery } from 'react-responsive';
+import {useMediaQuery} from 'react-responsive';
 import rootStores from '../../stores';
-import { SECTIONS_STORE } from '../../stores/consts';
+import {SECTIONS_STORE, STORE_INFORMATION_STORE} from '../../stores/consts';
 import SectionsStore from '../../stores/Sections.store';
+import StoreInformationStore from '../../stores/StoreInformation.store';
 import SideMenu from './sideMenu/SideMenu';
 
-const textOptions = ['משלוחים חינם בקנייה מעל 299 ש״ח', 'כל המוצרים בהנחות משוגעות', 'בעל הבית השתגע!'];
 const categoriesStore = rootStores[SECTIONS_STORE] as SectionsStore;
+const storeInformationStore = rootStores[STORE_INFORMATION_STORE] as StoreInformationStore;
 
 const Header = observer(() => {
-  const [index, setIndex] = useState(0);
-  const { getSections } = categoriesStore;
-  const isBigScreen = useMediaQuery({ query: '(min-width: 768px)' });
-  const isMobileScreen = useMediaQuery({ query: '(max-width: 768px)' });
+    const [index, setIndex] = useState(0);
+    const {getSections} = categoriesStore;
+    const {headerAddsTexts} = storeInformationStore;
+    const isBigScreen = useMediaQuery({query: '(min-width: 768px)'});
+    const isMobileScreen = useMediaQuery({query: '(max-width: 768px)'});
 
-  const navigate = useNavigate();
-  const onTitleClickHandler = () => navigate('/');
+    const navigate = useNavigate();
+    const onTitleClickHandler = () => navigate('/');
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % textOptions.length);
-    }, 5000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % headerAddsTexts.length);
+        }, 5000);
 
-    return () => clearInterval(interval);
-  }, []);
+        return () => clearInterval(interval);
+    }, [headerAddsTexts.length]);
 
-  useEffect(() => {
-    const fetchSections = async () => await getSections();
-    fetchSections().then(() => console.log('fetched sections...'));
-  }, []);
+    useEffect(() => {
+        const fetchSections = async () => await getSections();
+        fetchSections().then(() => console.log('fetched sections...'));
+    }, [getSections]);
 
-  const transitions = useTransition(textOptions[index], {
-    from: { opacity: 0, transform: 'translateX(400%)' },
-    enter: { opacity: 1, transform: 'translateX(0%)' },
-  });
+    const transitions = useTransition(headerAddsTexts[index], {
+        from: {opacity: 0, transform: 'translateX(400%)'},
+        enter: {opacity: 1, transform: 'translateX(0%)'},
+    });
 
-  return (
-    <>
-      <header className='header'>
-        <div className='header-text-container'>
-          {transitions((style, text) => (
-            <animated.p style={style} className='animated-text'>
-              {text}
-            </animated.p>
-          ))}
-        </div>
-      </header>
-      <div key={'header'} className={headerStyle.container}>
-        <div className={headerStyle.actionsCon}>
-          {isMobileScreen && <SideMenu />}
-          <SearchIcon />
-          <Title onClick={onTitleClickHandler} title={'SmokeTalk'} />
-          <HeaderActions />
-        </div>
-        {isBigScreen && <Sections />}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <header className='header'>
+                <div className='header-text-container'>
+                    {transitions((style, text) => (
+                        <animated.p style={style} className='animated-text'>
+                            {text}
+                        </animated.p>
+                    ))}
+                </div>
+            </header>
+            <div key={'header'} className={headerStyle.container}>
+                <div className={headerStyle.actionsCon}>
+                    {isMobileScreen && <SideMenu/>}
+                    <SearchIcon/>
+                    <Title onClick={onTitleClickHandler} title={'SmokeTalk'}/>
+                    <HeaderActions/>
+                </div>
+                {isBigScreen && <Sections/>}
+            </div>
+        </>
+    );
 });
 
 export default Header;
--- src/stores/StoreInformation.store.tsx
+++ src/stores/StoreInformation.store.tsx
@@ -1,103 +1,111 @@
-import { action, makeObservable, observable } from "mobx";
+import {action, makeObservable, observable} from "mobx";
 import ToastUtil from "../utils/ToastUtils";
 import {
-  IMediaLinks,
-  IInformation,
-  IOpeningHours,
-  IUsefulLinks,
-  IOurServices
+    IMediaLinks,
+    IInformation,
+    IOpeningHours,
+    IUsefulLinks,
+    IOurServices
 } from "../interfaces/interfaces";
 import StoreInformationFetcher from "../fetchers/StoreInformation.fetcher";
 
 class StoreInformationStore {
-  @observable
-  aboutText: string = "";
+    @observable
+    aboutText: string = "";
 
-  @observable
-  usefulLinks: IUsefulLinks[] = [];
+    @observable
+    usefulLinks: IUsefulLinks[] = [];
 
-  @observable
-  openingHours: IOpeningHours[] = [];
+    @observable
+    openingHours: IOpeningHours[] = [];
 
-  @observable
-  ourServices: IOurServices[] = [];
+    @observable
+    ourServices: IOurServices[] = [];
 
-  @observable
-  information: IInformation[] = [];
+    @observable
+    information: IInformation[] = [];
 
-  @observable
-  mediaLinks: IMediaLinks[] = [];
+    @observable
+    mediaLinks: IMediaLinks[] = [];
 
-  @observable
-  storeName: string = "";
+    @observable
+    storeName: string = "";
 
-  @observable
-  isLoading: boolean = false;
+    @observable
+    headerAddsTexts: string[] = [];
 
-  constructor() {
-    makeObservable(this);
-  }
+    @observable
+    isLoading: boolean = false;
 
-  @action
-  setAboutText = (aboutText: string) => (this.aboutText = aboutText);
-
-  @action
-  setUsefulLinks = (usefulLinks: IUsefulLinks[]) =>
-    (this.usefulLinks = usefulLinks);
-
-  @action
-  setOpeningHours = (openingHours: IOpeningHours[]) =>
-    (this.openingHours = openingHours);
-
-  @action
-  setInformation = (information: IInformation[]) =>
-    (this.information = information);
-
-  @action
-  setMediaLinks = (mediaLinks: IMediaLinks[]) => (this.mediaLinks = mediaLinks);
-
-  @action
-  setOurServices = (ourServices: IOurServices[]) => (this.ourServices = ourServices);
-
-  @action
-  setStoreName = (storeName: string) => (this.storeName = storeName);
-
-  @action
-  setIsLoading = (isLoading: boolean) => (this.isLoading = isLoading);
-
-  @action
-  getStoreInformation = async () => {
-    try {
-      this.setIsLoading(true);
-      let { data } = await StoreInformationFetcher.getStoreInformation();
-      this.setAboutText(data.aboutText);
-      this.setUsefulLinks(data.usefulLinks);
-      this.setOpeningHours(data.openingHours);
-      this.setInformation(data.information);
-      this.setMediaLinks(data.mediaLinks);
-      this.setStoreName(data.storeName);
-      this.setOurServices(data.ourServices)
-    } catch (err: any) {
-      console.error(err?.message);
-      ToastUtil.error("Some error occurred.");
-    } finally {
-      this.setIsLoading(false);
+    constructor() {
+        makeObservable(this);
     }
-  };
 
-  /*
     @action
-    updateCategory = async (categoryId, categoryName) => {
-      try {
-        await CategoriesFetcher.updateCategory(categoryId, categoryName);
-        await this.getCategories();
-      } catch (err: any) {
-        ToastUtil.error(err?.message);
-        console.error(err);
-      }
+    setAboutText = (aboutText: string) => (this.aboutText = aboutText);
+
+    @action
+    setUsefulLinks = (usefulLinks: IUsefulLinks[]) =>
+        (this.usefulLinks = usefulLinks);
+
+    @action
+    setOpeningHours = (openingHours: IOpeningHours[]) =>
+        (this.openingHours = openingHours);
+
+    @action
+    setInformation = (information: IInformation[]) =>
+        (this.information = information);
+
+    @action
+    setMediaLinks = (mediaLinks: IMediaLinks[]) => (this.mediaLinks = mediaLinks);
+
+    @action
+    setOurServices = (ourServices: IOurServices[]) => (this.ourServices = ourServices);
+
+    @action
+    setStoreName = (storeName: string) => (this.storeName = storeName);
+
+    @action
+    setHeaderAddsTexts = (texts: string[]) => (this.headerAddsTexts = texts);
+
+    @action
+    setIsLoading = (isLoading: boolean) => (this.isLoading = isLoading);
+
+    @action
+    getStoreInformation = async () => {
+        try {
+            this.setIsLoading(true);
+            let {data} = await StoreInformationFetcher.getStoreInformation();
+
+            this.setAboutText(data.aboutText);
+            this.setUsefulLinks(data.usefulLinks);
+            this.setOpeningHours(data.openingHours);
+            this.setInformation(data.information);
+            this.setMediaLinks(data.mediaLinks);
+            this.setStoreName(data.storeName);
+            this.setOurServices(data.ourServices)
+            this.setHeaderAddsTexts(data.headerAddsTexts)
+        } catch (err: any) {
+            console.error(err?.message);
+            ToastUtil.error("Some error occurred.");
+        } finally {
+            this.setIsLoading(false);
+        }
     };
 
-    */
+    /*
+      @action
+      updateCategory = async (categoryId, categoryName) => {
+        try {
+          await CategoriesFetcher.updateCategory(categoryId, categoryName);
+          await this.getCategories();
+        } catch (err: any) {
+          ToastUtil.error(err?.message);
+          console.error(err);
+        }
+      };
+
+      */
 }
 
 export default StoreInformationStore;
